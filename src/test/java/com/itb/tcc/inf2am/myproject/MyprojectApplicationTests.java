94a9e85ff92dc6d62b3b9dad8853180f89293bab
Rogério Caetano <rogercaetanos@gmail.com>
2023-08-24 07:49:54 -0300

    Criação do ambiente de desenvolvimento

---
+++ src/test/java/com/itb/tcc/inf2am/myproject/MyprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itb.tcc.inf2am.myproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
